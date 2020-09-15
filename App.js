ea6c23b09f90849f0da8daed22290169812e9f0b
anabcuelbas <abcuelbas@gmail.com>
2020-09-15 20:26:07 -0300

    Added custom fonts

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
+import * as Font from 'expo-font'
+import { AppLoading, appLoading } from 'expo'
 
 import Header from './components/Header'
 
@@ -7,9 +9,27 @@ import StartGameScreen from './screens/StartGameScreen'
 import GameScreen from './screens/GameScreen'
 import GameOverScreen from './screens/GameOverScreen'
 
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+  })
+}
+
 export default function App() {
   const [userNumber, setUserNumber] = useState()
   const [guessRounds, setGuessRounds] = useState(0)
+  const [dataLoaded, setDataLoaded] = useState(false)
+
+  if(!dataLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setDataLoaded(true)}
+        onError={(err) => console.log(err)} 
+      />
+    )
+  }
 
   const configureNewGameHandler = () => {
     setGuessRounds(0)
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -4,6 +4,8 @@ import { View, Text, StyleSheet, Button, TouchableWithoutFeedback, Keyboard, Ale
 import Card from '../components/Card'
 import Input from '../components/Input'
 import NumberContainer from '../components/NumberContainer'
+import BodyText from '../components/BodyText'
+import TitleText from '../components/TitleText'
 import Colors from '../constants/colors'
 
 const StartGameScreen = props => {
@@ -38,9 +40,9 @@ const StartGameScreen = props => {
     if(confirmed) {
         confirmedOutput = (
             <Card style={styles.summaryContainer}>
-                <Text>You selected</Text>
-            <NumberContainer>{selectedNumber}</NumberContainer>
-            <Button title="START GAME" onPress={() => props.onStartGame(selectedNumber)} />
+                <BodyText>You selected</BodyText>
+                <NumberContainer>{selectedNumber}</NumberContainer>
+                <Button title="START GAME" onPress={() => props.onStartGame(selectedNumber)} />
             </Card>
         )
     }
@@ -50,9 +52,9 @@ const StartGameScreen = props => {
             Keyboard.dismiss()
         }}>
             <View style={styles.screen}>
-                <Text style={styles.title}>Start a New Game!</Text>
+                <TitleText style={styles.title}>Start a New Game!</TitleText>
                 <Card style={styles.inputContainer}>
-                    <Text>Select a Number</Text>
+                    <BodyText>Select a Number</BodyText>
                     <Input style={styles.input} 
                         blurOnSubmit 
                         autoCapitalize="none" 
@@ -85,7 +87,8 @@ const styles = StyleSheet.create({
     },
     title: {
         fontSize: 20,
-        marginVertical: 10
+        marginVertical: 10,
+        fontFamily: 'open-sans-bold'
     },
     inputContainer: {
         width: 300,
@@ -108,6 +111,9 @@ const styles = StyleSheet.create({
     summaryContainer: {
         marginTop: 20,
         alignItems: 'center'
+    },
+    text: {
+        fontFamily: 'open-sans'
     }
 })
 
